810f85d6b27a809d47539304494b5947c48d9bad
Joseph Hartono <36626772+EvilCrayFish@users.noreply.github.com>
2024-09-23 22:24:50 +1000

    Class Definitions for Staff and Teacher classes

---
+++ classes/cls_staff.py
@@ -0,0 +1,33 @@
+"""
+FIT1056 2024 Semester 2
+EmpowerU Project
+
+This file contains the class definition for the staff class.
+"""
+
+
+
+class Staff:
+    def __init__(self, staff_id, name, email, phone, role):
+        """
+        Constructor for the User class.
+        """
+        self.staff_id = staff_id
+        self.name = name
+        self.email = email
+        self.phone = phone
+        self.role = role
+
+    def login(user_name, password):
+        #TODO Write method
+        pass
+    def updateProfile():
+        #TODO Write method
+        pass
+    def returnProfile():
+        #TODO Write method
+        pass
+
+
+if __name__ == "__main__":
+    pass
