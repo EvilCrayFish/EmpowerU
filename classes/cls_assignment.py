73dfa7e5f0a466d1a27fca57d8c4472820768abb
fenrir51 <aelnick@hotmail.com>
2024-10-17 14:50:30 +1100

    Create cls_assignment.py

---
+++ classes/cls_assignment.py
@@ -0,0 +1,11 @@
+"""
+FIT1056 2024 Semester 2
+EmpowerU Project
+
+This file contains the class definition for the assignment class.
+"""
+
+class Assignment:
+    def __init__(self, name, status):
+        self.name = name
+        self.status = status
