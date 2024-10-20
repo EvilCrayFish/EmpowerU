9d0b2b2e7383a25ce238b0b394f9d2b3b06bcc4d
mlyn01 <mlyn0006@student.monash.edu>
2024-10-20 11:14:02 +1100

    Added new_lessons.txt and added more tests

--- tests/test_edit_lesson_page.py
+++ tests/test_edit_lesson_page.py
@@ -1,5 +1,30 @@
 "Testing for the functionalities within the edit lesson page for EmpowerU"
 
+from interfaces.gui_edit_lesson_page import EditLessonPage
 
 def get_lesson_line():
-    pass
+    
+
+    page1 = EditLessonPage()
+    assert page1.get_lesson_line() == []
+
+    page2 = EditLessonPage()
+    assert page2.get_lesson_line() == []
+
+    page3 = EditLessonPage()
+    assert page3.get_lesson_line() == []
+
+    page4 = EditLessonPage()
+    assert page4.get_lesson_line() == []
+
+    page5 = EditLessonPage()
+    assert page5.get_lesson_line() == []
+
+    page6 = EditLessonPage()
+    assert page6.get_lesson_line() == []
+
+    page7 = EditLessonPage()
+    assert page7.get_lesson_line() == []
+
+    page8 = EditLessonPage()
+    assert page8.get_lesson_line() == []
--- tests/test_lesson_page.py
+++ tests/test_lesson_page.py
@@ -1,5 +1,29 @@
 "Testing for the functionalities within the Progress tracker page for EmpowerU"
 
+from interfaces.gui_edit_lesson_page import EditLessonPage
 
 def test_get_lesson_line():
-    pass
+    
+    page1 = EditLessonPage()
+    assert page1.get_lesson_line() == []
+
+    page2 = EditLessonPage()
+    assert page2.get_lesson_line() == []
+
+    page3 = EditLessonPage()
+    assert page3.get_lesson_line() == []
+
+    page4 = EditLessonPage()
+    assert page4.get_lesson_line() == []
+
+    page5 = EditLessonPage()
+    assert page5.get_lesson_line() == []
+
+    page6 = EditLessonPage()
+    assert page6.get_lesson_line() == []
+
+    page7 = EditLessonPage()
+    assert page7.get_lesson_line() == []
+
+    page8 = EditLessonPage()
+    assert page8.get_lesson_line() == []
